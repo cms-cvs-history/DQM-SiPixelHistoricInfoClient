2099a5838df395e4426666ce4ed804a4f0c1bae6
schuang <sha1-65eb9735417a340a7fc69881308d83c349d3a9ce@cern.ch>
2008-11-04 17:05:53 +0000

    changed lastRun, DB connect string and performanceSummary tag name

--- test/runSiPixelHistoricInfoDQMClient_harvest_cfg.py
+++ test/runSiPixelHistoricInfoDQMClient_harvest_cfg.py
@@ -8,30 +8,32 @@ process.source = cms.Source("EmptyIOVSource",
   timetype = cms.string('runnumber'),
   interval = cms.uint32(1),
   firstRun = cms.untracked.uint32(54751),
-   lastRun = cms.untracked.uint32(64800)
+   lastRun = cms.untracked.uint32(99999)
 )
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 process.CondDBSetup.DBParameters.authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
   process.CondDBSetup, 
-  # connect = cms.string('oracle://cms_orcoff_prep/CMS_COND_PIXEL'), 
-  connect = cms.string('sqlite_file:testPixelHistory.db'),
+  connect = cms.string('oracle://cms_orcoff_prep/CMS_COND_PIXEL_COMM_21X'), 
   timetype = cms.untracked.string('runnumber'),
   toPut = cms.VPSet(
     cms.PSet(
       record = cms.string('SiPixelPerformanceSummaryRcd'),
-      tag = cms.string('SiPixelPerformanceSummary_test')
+      tag = cms.string('SiPixelPerformanceSummary_DQM')
     )
   )
 )
 process.sipixelhistoricinfoDQMclient = cms.EDFilter("SiPixelHistoricInfoDQMClient",
   inputFiles = cms.untracked.vstring(
-    'DQMoutput/DQM_PhysicsData_Run62938.root',
-    'DQMoutput/DQM_PhysicsData_Run62940.root',
+    'DQMoutput/DQM_PhysicsData_Run54768.root'
   ),
   outputDir = cms.untracked.string('.'),
   useSummary = cms.untracked.bool(True),
   printDebug = cms.untracked.bool(False),
   writeHisto = cms.untracked.bool(False)
+)
+process.DQMStore = cms.Service("DQMStore",
+  referenceFileName = cms.untracked.string(''),
+  verbose = cms.untracked.int32(0)
 )
 process.pathAll = cms.Path(process.sipixelhistoricinfoDQMclient)
