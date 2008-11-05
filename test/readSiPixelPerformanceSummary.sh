8ba41d2c1d154eaedeec5a5ca30b5f5f3234176b
schuang <sha1-65eb9735417a340a7fc69881308d83c349d3a9ce@cern.ch>
2008-11-05 11:56:00 +0000

    bash scripts to make DB-reading cfg.py out of template and run it

---
+++ test/readSiPixelPerformanceSummary.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+filetype="_"$1
+
+template="readSiPixelPerformanceSummary_template_cfg.py"
+perfsumTag="SiPixelPerformanceSummary"$filetype
+
+  if [ $filetype = "_DQM" -o $filetype = "_RAW" -o $filetype = "_RECO" ]; then
+  sed -e "s#perfsumtag#$perfsumTag#" $template > readSiPixelPerformanceSummary_data_cfg.py
+  cmsRun readSiPixelPerformanceSummary_data_cfg.py
+
+elif [ $filetype = "_MC" ]; then 
+  sed -e "s#oracle://cms_orcoff_prep/CMS_COND_PIXEL_COMM_21X#sqlite_file:SiPixelHistory_MC.db#" \
+      -e "s#perfsumtag#$perfsumTag#" $template > readSiPixelPerformanceSummary_mc_cfg.py
+  cmsRun readSiPixelPerformanceSummary_mc_cfg.py
+
+else
+  echo 
+  echo "  USAGE:"
+  echo "./readSiPixelPerformanceSummary.sh [filetype]"
+  echo "  where "
+  echo " [filetype] can be either DQM, RAW, RECO or MC."
+  echo
+fi
+
