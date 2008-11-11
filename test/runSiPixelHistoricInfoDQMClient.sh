c9b090161cb2b920f4757230bb011b2d4597cac4
schuang <sha1-65eb9735417a340a7fc69881308d83c349d3a9ce@cern.ch>
2008-11-11 17:50:50 +0000

    bash scripts to run make DB-writing cfg.py out of template_perFile and run it

---
+++ test/runSiPixelHistoricInfoDQMClient.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+eval `scramv1 ru -sh`
+
+filelist=$1 
+floss="_"$1
+
+if [ $floss = "_" ]; then
+  echo 
+  echo "  USAGE:"
+  echo "./runSiPixelHistoricInfoClient.sh [filelist]"
+  echo "  where "
+  echo "  the files must be DQM (PhysicsData on AFS)"
+  echo 
+else
+  for file in $(cat $filelist); do 
+    midRun=`echo $file | awk '{print substr($1,match($1,"R0000")+5,5)}'`    
+    firstRun=$[midRun-1]
+    lastRun=$[midRun+1]
+    
+    inputFile="'"$file"'"
+
+    template=runSiPixelHistoricInfoDQMClient_template_perFile_cfg.py
+    sed -e "s#DQMoutputFiles#$inputFile#" \
+        -e "s#FFFFF#$firstRun#" \
+	-e "s#LLLLL#$lastRun#" $template > runSiPixelHistoricInfoDQMClient_dqm_cfg.py
+    # cmsRun runSiPixelHistoricInfoDQMClient_dqm_cfg.py
+  done 
+fi
